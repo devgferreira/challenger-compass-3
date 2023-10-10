ce2abd5893c34e0a466b0fd51909531d30323599
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-10 13:10:07 -0300

    feat: criando o dto: FuncionarioDTO

--- ms-funcionario/src/main/java/br/gabriel/souto/msfuncionario/domain/model/funcionario/Funcionario.java
+++ ms-funcionario/src/main/java/br/gabriel/souto/msfuncionario/domain/model/funcionario/Funcionario.java
@@ -1,6 +1,9 @@
 package br.gabriel.souto.msfuncionario.domain.model.funcionario;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String cpf;
     String nome;
