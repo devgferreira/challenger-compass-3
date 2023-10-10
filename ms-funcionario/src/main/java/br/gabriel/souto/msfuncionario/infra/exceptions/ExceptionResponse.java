d0a835730a165f3105ab64e667ec9e26ca717d39
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-10 16:59:00 -0300

    feat: criando o ExceptionResponse

---
+++ ms-funcionario/src/main/java/br/gabriel/souto/msfuncionario/infra/exceptions/ExceptionResponse.java
@@ -0,0 +1,21 @@
+package br.gabriel.souto.msfuncionario.infra.exceptions;
+
+import br.gabriel.souto.msfuncionario.domain.enums.ErrorCodes;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ExceptionResponse {
+    private final String code;
+    private final String message;
+    public ExceptionResponse(final ErrorCodes errorCode, String details) {
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+
+    public ExceptionResponse(ErrorCodes errorCode, List<String> details) {
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+}
