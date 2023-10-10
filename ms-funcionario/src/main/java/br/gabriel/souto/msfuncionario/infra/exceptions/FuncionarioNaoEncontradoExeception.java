c8cd3161a0d20e12b32469cb73497b08840ee98d
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-10 16:59:50 -0300

    feat: criando a execeptions para funcionario não encontrado: FuncionarioNaoEncontradoExeception

---
+++ ms-funcionario/src/main/java/br/gabriel/souto/msfuncionario/infra/exceptions/FuncionarioNaoEncontradoExeception.java
@@ -0,0 +1,14 @@
+package br.gabriel.souto.msfuncionario.infra.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class FuncionarioNaoEncontradoExeception extends RuntimeException{
+    private ExceptionResponse exceptionResponse;
+
+    public FuncionarioNaoEncontradoExeception(ExceptionResponse exceptionResponse) {
+        super();
+        this.exceptionResponse = exceptionResponse;
+    }
+}
