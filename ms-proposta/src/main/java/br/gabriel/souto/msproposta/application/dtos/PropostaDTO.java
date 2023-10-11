a7b23896058e77b107dce0e7ed5e26696368f7a3
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-11 15:32:22 -0300

    feat: criando o dto: PropostaDTO

---
+++ ms-proposta/src/main/java/br/gabriel/souto/msproposta/application/dtos/PropostaDTO.java
@@ -0,0 +1,14 @@
+package br.gabriel.souto.msproposta.application.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PropostaDTO {
+    Long id;
+    String titulo;
+    String Descricao;
+}
