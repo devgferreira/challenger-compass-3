15656ce4990f121ab324190a951f153e045ba87b
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-11 15:33:27 -0300

    feat: criando a interface: IPropostaService, juntamente com os contratos: criarProposta e buscarPropostaPorId

---
+++ ms-proposta/src/main/java/br/gabriel/souto/msproposta/application/interfaces/IPropostaService.java
@@ -0,0 +1,8 @@
+package br.gabriel.souto.msproposta.application.interfaces;
+
+import br.gabriel.souto.msproposta.application.dtos.PropostaDTO;
+
+public interface IPropostaService {
+    PropostaDTO criarProposta(PropostaDTO propostaDTO);
+    PropostaDTO buscarPropostaPorId(Long id);
+}
