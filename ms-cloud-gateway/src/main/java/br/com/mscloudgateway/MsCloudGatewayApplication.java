90ea850f1f9c13037d95014f95fb1b7d8deb5f61
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-11 15:55:22 -0300

    feate: configurando o geteway

---
+++ ms-cloud-gateway/src/main/java/br/com/mscloudgateway/MsCloudGatewayApplication.java
@@ -0,0 +1,25 @@
+package br.com.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/funcionarios/**").uri("lb://ms-clientes"))
+				.route(r -> r.path("/propostas/**").uri("lb://ms-cartoes"))
+				.build();
+	}
+}
