4c6ac1c9c5fe8a8720fddc77f7c6467641055150
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-12 00:15:26 -0300

    feat: criando o Enum ErrorConstants

---
+++ ms-proposta/src/main/java/br/gabriel/souto/msproposta/infra/constants/ErrorConstants.java
@@ -0,0 +1,9 @@
+package br.gabriel.souto.msproposta.infra.constants;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter(AccessLevel.PRIVATE)
+public class ErrorConstants {
+    public static final String PROPOSTA_NAO_ENCONTRADA = "[MS-PROPOSTA] Proposta não foi encontrado";
+}
