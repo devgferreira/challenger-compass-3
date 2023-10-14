203ceb9d66fa778cedbb95069b478fd5c70544f7
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-14 11:08:52 -0300

    feat: estrutura inicial

---
+++ ms-validacao-voto/src/main/java/br/gabriel/souto/msvalidacaovoto/MsValidacaoVotoApplication.java
@@ -0,0 +1,13 @@
+package br.gabriel.souto.msvalidacaovoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsValidacaoVotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsValidacaoVotoApplication.class, args);
+	}
+
+}
