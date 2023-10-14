7a27780752d281430505683a8f075a10d6fc4cc5
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-14 11:50:41 -0300

    feat: adicionando a anotação @EnableFeignClients no MsValidacaoVotoApplication

--- ms-validacao-voto/src/main/java/br/gabriel/souto/msvalidacaovoto/MsValidacaoVotoApplication.java
+++ ms-validacao-voto/src/main/java/br/gabriel/souto/msvalidacaovoto/MsValidacaoVotoApplication.java
@@ -2,8 +2,10 @@ package br.gabriel.souto.msvalidacaovoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsValidacaoVotoApplication {
 
 	public static void main(String[] args) {
