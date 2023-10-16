fe64c9e22dd2027778b4199744f23b79108e5f1d
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-16 17:46:49 -0300

    feat: adicionando a dependencia do RabbitMQ no ms-voto e ms-proposta

--- ms-voto/src/main/java/br/gabriel/souto/msvoto/MsVotoApplication.java
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/MsVotoApplication.java
@@ -1,9 +1,13 @@
 package br.gabriel.souto.msvoto;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableRabbit
+@EnableFeignClients
 public class MsVotoApplication {
 
 	public static void main(String[] args) {
