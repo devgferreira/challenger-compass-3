3c9068586f8d4d527e482fda42413dc00dc586f9
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-16 17:45:07 -0300

    feat: criando interface do OpenFeign da Proposta: PropostaControllerClient

---
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/infra/client/PropostaControllerClient.java
@@ -0,0 +1,12 @@
+package br.gabriel.souto.msvoto.infra.client;
+
+import br.gabriel.souto.msvoto.domain.model.proposta.Proposta;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "ms-proposta", path = "/propostas")
+public interface PropostaControllerClient {
+    @GetMapping("/{id}")
+    Proposta buscarPropostaPorId(@PathVariable Long id);
+}
