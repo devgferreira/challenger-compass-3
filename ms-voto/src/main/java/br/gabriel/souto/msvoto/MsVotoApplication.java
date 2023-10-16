8b773fcec9c9083033ba2af42cf4e4537976167f
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-16 15:29:36 -0300

    feat: estrutura inicial

---
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/MsVotoApplication.java
@@ -0,0 +1,13 @@
+package br.gabriel.souto.msvoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVotoApplication.class, args);
+	}
+
+}
