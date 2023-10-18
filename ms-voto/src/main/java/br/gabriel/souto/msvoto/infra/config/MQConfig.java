a11b8464bd30ebf104be70b137121117592cda58
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-18 14:08:23 -0300

    feat: Configurando a nova fila do RabbitMQ: queueEmitirResultadoVotacao

--- ms-voto/src/main/java/br/gabriel/souto/msvoto/infra/config/MQConfig.java
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/infra/config/MQConfig.java
@@ -1,4 +1,4 @@
-package br.gabriel.souto.msvoto.domain.config;
+package br.gabriel.souto.msvoto.infra.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +9,14 @@ import org.springframework.context.annotation.Configuration;
 public class MQConfig {
     @Value("${mq.queues.emitir-proposta-resultado-votacao}")
     private String emitirPropostaResultadoVotacaoFila;
+    @Value("${mq.queues.emitir-resultado-votacao}")
+    private String emitirResultadoVotacaoFila;
     @Bean
     public Queue queueEmitirPropostaResultado(){
         return new Queue(emitirPropostaResultadoVotacaoFila, true);
+    }
+    @Bean
+    public Queue queueEmitirResultadoVotacao(){
+        return new Queue(emitirResultadoVotacaoFila, true);
     }
 }
