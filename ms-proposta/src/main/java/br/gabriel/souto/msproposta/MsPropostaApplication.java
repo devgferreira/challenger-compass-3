18f00b528ee177eb103733ee0e6cddff4d509da6
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-18 12:20:44 -0300

    feat: adicioando a anotação @EnableScheduling no MsPropostaApplication

--- ms-proposta/src/main/java/br/gabriel/souto/msproposta/MsPropostaApplication.java
+++ ms-proposta/src/main/java/br/gabriel/souto/msproposta/MsPropostaApplication.java
@@ -3,9 +3,11 @@ package br.gabriel.souto.msproposta;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableScheduling
 public class MsPropostaApplication {
 
 	public static void main(String[] args) {
