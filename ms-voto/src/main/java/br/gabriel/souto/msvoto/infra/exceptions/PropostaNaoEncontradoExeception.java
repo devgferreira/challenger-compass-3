19edfaa6e9c3a1f9e721d9a1346e44c1b0fb846d
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-18 22:23:30 -0300

    feat: criando a PropostaNaoEncontradoExeception

---
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/infra/exceptions/PropostaNaoEncontradoExeception.java
@@ -0,0 +1,14 @@
+package br.gabriel.souto.msvoto.infra.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PropostaNaoEncontradoExeception extends RuntimeException{
+    private ExceptionResponse exceptionResponse;
+
+    public PropostaNaoEncontradoExeception(ExceptionResponse exceptionResponse) {
+        super();
+        this.exceptionResponse = exceptionResponse;
+    }
+}
