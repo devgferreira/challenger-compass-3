636abb1b753f83800a913137817ba410bbaa5da2
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-18 22:22:03 -0300

    feat: criando a ExceptionResponse

---
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/infra/exceptions/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package br.gabriel.souto.msvoto.infra.exceptions;
+
+import br.gabriel.souto.msvoto.domain.enums.ErrorCodes;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ExceptionResponse {
+    private final String code;
+    private final String message;
+    public ExceptionResponse(final ErrorCodes errorCode, String details) {
+        this.code = errorCode.name();
+        this.message = errorCode.getMessage();
+    }
+
+
+}
