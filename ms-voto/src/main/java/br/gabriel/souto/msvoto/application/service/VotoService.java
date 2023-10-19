138a8827355e7e4238317090903a2955a41054c7
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-18 22:24:46 -0300

    feat: implementando o tratamento de exceções na service: VotoService

--- ms-voto/src/main/java/br/gabriel/souto/msvoto/application/service/VotoService.java
+++ ms-voto/src/main/java/br/gabriel/souto/msvoto/application/service/VotoService.java
@@ -1,6 +1,7 @@
 package br.gabriel.souto.msvoto.application.service;
 
 import br.gabriel.souto.msvoto.application.interfaces.IVotoService;
+import br.gabriel.souto.msvoto.domain.enums.ErrorCodes;
 import br.gabriel.souto.msvoto.domain.enums.VotoStatus;
 import br.gabriel.souto.msvoto.domain.interfaces.IVotoRepository;
 import br.gabriel.souto.msvoto.domain.model.funcionario.Funcionario;
@@ -9,9 +10,12 @@ import br.gabriel.souto.msvoto.domain.model.voto.Voto;
 import br.gabriel.souto.msvoto.infra.client.FuncionarioControllerClient;
 import br.gabriel.souto.msvoto.infra.client.PropostaControllerClient;
 import br.gabriel.souto.msvoto.infra.client.ValidacaoVotoControllerClient;
+import br.gabriel.souto.msvoto.infra.constants.ErrorConstants;
+import br.gabriel.souto.msvoto.infra.exceptions.*;
 import br.gabriel.souto.msvoto.infra.mqueue.EmitirPropostaResultadoPublisher;
 import br.gabriel.souto.msvoto.infra.mqueue.EmitirResultadoVotacaoPublisher;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -44,21 +48,37 @@ public class VotoService implements IVotoService {
 
     @Override
     public void votar(String funcionarioCpf, Long propostaId, VotoStatus status) throws JsonProcessingException {
-        Funcionario funcionario = _funcionarioControllerClient.buscarFuncionarioPorCpf(funcionarioCpf);
-        Proposta proposta = _propostaControllerClient.buscarPropostaPorId(propostaId);
+        Funcionario funcionario;
+        Proposta proposta;
+        try {
+             funcionario = _funcionarioControllerClient.buscarFuncionarioPorCpf(funcionarioCpf);
+        }
+        catch (FeignException.NotFound ex){
+            throw new FuncionarioNaoEncontradoExeception(new ExceptionResponse(
+                    ErrorCodes.FUNCIONARIO_NAO_ENCONTRADO, ErrorConstants.FUNCIONARIO_NAO_ENCONTRADO));
+        }
+        try{
+            proposta = _propostaControllerClient.buscarPropostaPorId(propostaId);
+
+        }catch (FeignException.NotFound ex){
+            throw new PropostaNaoEncontradoExeception(new ExceptionResponse(
+                    ErrorCodes.PROPOSTA_NAO_ENCONTRADA, ErrorConstants.PROPOSTA_NAO_ENCONTRADA
+            ));
+        }
         String voto_valido = _validacaoVotoControllerClient.validarVoto(propostaId, funcionarioCpf);
 
         if(voto_valido.equals("nao_pode_votar")){
-            throw new RuntimeException("O Funcionario não pode votar nessa proposta");
+            throw new NaoPodeVotarExeception(new ExceptionResponse(ErrorCodes.NAO_PODE_VOTAR, ErrorConstants.NAO_PODE_VOTAR));
         }
 
         if(proposta.isAberta()){
             Optional<Voto> votoExistente = _votoRepository.findByFuncionarioCpfAndPropostaId(
                     funcionario.getCpf(), proposta.getId());
             if(votoExistente.isPresent()){
-                throw new RuntimeException("Funcionario já votou nessa proposta");
+                throw new FuncionarioJaVotoExeception(new ExceptionResponse(
+                        ErrorCodes.FUNCIONARIO_JA_VOTO, ErrorConstants.FUNCIONARIO_JA_VOTO
+                ));
             }
-
             Voto novoVoto = new Voto();
             novoVoto.setFuncionarioCpf(funcionario.getCpf());
             novoVoto.setPropostaId(proposta.getId());
@@ -68,7 +88,9 @@ public class VotoService implements IVotoService {
         }
 
         if(!proposta.isAberta()){
-            throw new RuntimeException("A votação foi encerrada!");
+            throw new VotacaoEncerradaExeception(new ExceptionResponse(
+                    ErrorCodes.VOTACAO_ENCERRADA, ErrorConstants.VOTACAO_ENCERRADA
+            ));
         }
 
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
