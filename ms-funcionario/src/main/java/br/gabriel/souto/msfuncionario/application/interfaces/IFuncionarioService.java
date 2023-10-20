794d19ef6d321317b1ee61e1818b2132f142eb86
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-10-20 10:52:51 -0300

    test: validando cpfs

--- ms-funcionario/src/main/java/br/gabriel/souto/msfuncionario/application/interfaces/IFuncionarioService.java
+++ ms-funcionario/src/main/java/br/gabriel/souto/msfuncionario/application/interfaces/IFuncionarioService.java
@@ -5,4 +5,5 @@ import br.gabriel.souto.msfuncionario.application.dtos.FuncionarioDTO;
 public interface IFuncionarioService {
     FuncionarioDTO criarFuncionario( FuncionarioDTO funcionarioDTO);
     FuncionarioDTO buscarFuncionarioPorCpf(String cpf);
+    boolean validator(String cpf);
 }
